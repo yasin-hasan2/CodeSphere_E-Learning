cd6e42b4b2d64188da9e2cd31dea420179bc35c4
Yasin <yaseenalhassan2@gmail.com>
2025-12-31 13:15:15 +0600

    setup diploy mode

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,10 @@ import connectDb from "./database/db.js";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
+// import routes
+
 import userRoutes from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
@@ -16,18 +20,27 @@ connectDb();
 const app = express();
 const PORT = process.env.PORT || 6000;
 
+const __dirname = path.resolve();
+
 // Default middleware
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://127.0.0.1:5173",
+  "http://localhost:6001",
+];
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(null, false); // ❗ NO ERROR THROW
       }
     },
     credentials: true,
@@ -48,6 +61,29 @@ app.get("/home", (req, res) => {
     message: "Welcome to CodeSphere LMS",
   });
 });
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  const clientPath = path.join(__dirname, "../client/dist");
+
+  app.use(express.static(clientPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
+// else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 
 // start the server
 
