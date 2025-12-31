cd6fb8bb0c3a23c31e29647b4f87a83836b846a6
Yasin <yaseenalhassan2@gmail.com>
2025-12-31 23:49:51 +0600

    update api

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-// const USER_API =
-//   import.meta.env.MODE === "development"
-//     ? "http://localhost:6001/api/v1/user/"
-//     : "https://codesphere-lms.onrender.com/api/v1/user/";
+const USER_API =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:6001/api/v1/user/"
+    : "https://codesphere-lms.onrender.com/api/v1/user/";
 
-const USER_API = "http://localhost:6001/api/v1/user/";
+// const USER_API = "http://localhost:6001/api/v1/user/";
 
 export const authApi = createApi({
   reducerPath: "authApi",
